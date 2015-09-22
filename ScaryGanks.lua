d8a5e8fbe5b6baaa63e322aa780133e2a4bd461a
ShivMOD <ricohet@op.pl>
2015-09-22 02:56:57 +0200

    ganked

--- ScaryGanks.lua
+++ ScaryGanks.lua
@@ -12,7 +12,7 @@ if GoS:EnemiesAround(GoS:myHeroPos(), 4000) > 0 then
 				local myscreenpos = WorldToScreen(1,hero_origin.x,hero_origin.y,hero_origin.z)
 				
 					if GoS:EnemiesAround(GoS:myHeroPos(), 4000) > GoS:AlliesAround(GoS:myHeroPos(),2000)+1 then
-						DrawText(string.format("YOU ARE GETTING GANKED!"),24,myscreenpos.x,myscreenpos.y,0xffff0000)
+						DrawText(string.format("GANKED!"),24,myscreenpos.x,myscreenpos.y,0xffff0000)
 					
 				end
 			end
