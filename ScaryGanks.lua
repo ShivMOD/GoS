65d534fe08dea1d077b21d28c1f09362060f745a
ShivMOD <ricohet@op.pl>
2015-09-22 02:41:06 +0200

    v1.0
    Fixed all problems

--- ScaryGanks.lua
+++ ScaryGanks.lua
@@ -1,15 +1,14 @@
 require('Inspired')
-PrintChat("ScaryGanks by SHIV - loaded")
+PrintChat("ScaryGanks by Shiv v1.0")
 ScaryGanksMenu = Menu("ScaryGanks", "ScaryGanks")
 ScaryGanksMenu:SubMenu("Ganking", "Ganks")
 ScaryGanksMenu.Ganking:Boolean("Alert", "Show Alerts", true)
 
 OnLoop(function(myHero)
-local myHeroPos = GetOrigin(myHero)
 if ScaryGanksMenu.Ganking.Alert:Value()	then
 if GoS:EnemiesAround(GoS:myHeroPos(), 4000) > 0 then
 			
-				local hero_origin = myHeroPos
+				local hero_origin = GoS:myHeroPos()
 				local myscreenpos = WorldToScreen(1,hero_origin.x,hero_origin.y,hero_origin.z)
 				
 					if GoS:EnemiesAround(GoS:myHeroPos(), 4000) > GoS:AlliesAround(GoS:myHeroPos(),2000)+1 then
